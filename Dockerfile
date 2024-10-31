f65adef3037c51c6cfdbd60a2891602a6b7d568d
root <reginaa_barbara@hotmail.com>
2024-10-31 19:24:29 -0300

    envs

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+FROM node:22 as build
+
+ARG NODE_ENV
+ENV NODE_ENV=${NODE_ENV}
+
+ARG mode
+ENV VITE_MODE=${mode}
+
+
+WORKDIR /app/
+
+COPY package*.json /app/
+
+RUN npm install --prefer-offline --no-audit --progress=false
+
+COPY . ./app/
+
+RUN npm run build
+
+FROM nginx:alpine
+
+COPY --from=build /app/dist /usr/share/nginx/html
+
+EXPOSE 80
+
+CMD [ "nginx", "-g", "daemon off;" ]
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+
+//       <h1>React Project</h1>
+
+      
+
+
+
+
+
+//     </>
+//   )
+// }
+
+// export default App
+import React from 'react';
+
+const App = () => {
+  
+  const mode = import.meta.env.VITE_MODE;
+
+  const env = process.env.NODE_ENV;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React Project</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Bem-vindo ao meu aplicativo!</h1>
+      <p>Ambiente: {env}</p>
+      <p>mode:{mode}</p>
+    </div>
+  );
+};
 
-export default App
+export default App;
