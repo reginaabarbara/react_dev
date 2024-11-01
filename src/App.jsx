1aaac3c2d41bcef1682a6361daddc694d810b82e
root <root@DESKTOP-S5N8AU0.>
2024-11-01 19:13:58 -0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -26,13 +26,13 @@ const App = () => {
   
   const mode = import.meta.env.VITE_MODE;
 
-  const env = process.env.NODE_ENV;
+  // const env = process.env.NODE_ENV;
 
   return (
     <div>
       <h1>Bem-vindo ao meu aplicativo!</h1>
       {/* <p>Ambiente: {env}</p> */}
-      <p>mode:{mode}</p>
+      <p>Variavel "mode" == Branch GITHUB: {mode}</p>
     </div>
   );
 };
