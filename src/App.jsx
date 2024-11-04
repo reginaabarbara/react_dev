4e1d22a99a2ca1c20a8fa976bccbd172061001a0
root <root@DESKTOP-S5N8AU0.>
2024-11-04 11:47:43 -0300

    teste operação

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 
-
 import React from 'react';
 
 const App = () => {
   
   const mode = import.meta.env.VITE_MODE;
 
-  // const env = process.env.NODE_ENV;
+//  const env = process.env.NODE_ENV;
 
   return (
     <div>
       <h1>Bem-vindo ao meu aplicativo operação!</h1>
-      <p>Ambiente: {env}</p>
+      {/* <p>Ambiente: {env}</p> */}
       <p>mode:{mode}</p>
     </div>
   );
